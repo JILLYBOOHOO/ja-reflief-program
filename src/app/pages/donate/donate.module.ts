9d1bed0467691d70706d1085b66c41c3a167e5bf
Jillyboohoo <truth147.90@gmail.com>
2026-04-10 11:16:30 -0500

    Fix: Replace QRCodeModule with QRCodeComponent in DonateModule to resolve compilation error

--- src/app/pages/donate/donate.module.ts
+++ src/app/pages/donate/donate.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DonateComponent } from './donate.component';
 import { SharedModule } from '../../shared/shared.module';
-import { QRCodeModule } from 'angularx-qrcode';
+import { QRCodeComponent } from 'angularx-qrcode';
 import { GoogleSigninButtonModule } from '@abacritt/angularx-social-login';
 
 const routes: Routes = [{ path: '', component: DonateComponent }];
@@ -16,7 +16,7 @@ const routes: Routes = [{ path: '', component: DonateComponent }];
     FormsModule,
     ReactiveFormsModule,
     SharedModule,
-    QRCodeModule,
+    QRCodeComponent,
     GoogleSigninButtonModule,
     RouterModule.forChild(routes)
   ]
